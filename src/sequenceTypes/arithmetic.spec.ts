c8fa4ddf659103d54e84ffd407b9377f6d22ee20
Mike Hogarth <mikehogarth20@hotmail.com>
2020-09-02 22:51:46 +0100

    Update tests

--- src/sequenceTypes/arithmetic.spec.ts
+++ src/sequenceTypes/arithmetic.spec.ts
@@ -1,25 +1,23 @@
 import Arithmetic from "./arithmetic";
 
 describe("Arithmetic Sequences", () => {
-  describe("generate", () => {
-    it("creates an arithmetic sequence", () => {
-      const seq = Arithmetic(1, 1);
-      expect(seq.nextN(5)).toEqual([1, 2, 3, 4, 5]);
-    });
+  it("creates an arithmetic sequence", () => {
+    const seq = Arithmetic(1, 1);
+    expect(seq.nextN(5)).toEqual([1, 2, 3, 4, 5]);
+  });
 
-    it("allows you to specify a start number", () => {
-      const seq = Arithmetic(2, 1);
-      expect(seq.nextN(5)).toEqual([2, 3, 4, 5, 6]);
-    });
+  it("allows you to specify a start number", () => {
+    const seq = Arithmetic(2, 1);
+    expect(seq.nextN(5)).toEqual([2, 3, 4, 5, 6]);
+  });
 
-    it("allows you to specify the common difference", () => {
-      const seq = Arithmetic(2, 2);
-      expect(seq.nextN(5)).toEqual([2, 4, 6, 8, 10]);
-    });
+  it("allows you to specify the common difference", () => {
+    const seq = Arithmetic(2, 2);
+    expect(seq.nextN(5)).toEqual([2, 4, 6, 8, 10]);
+  });
 
-    it("allows you to set negative values for the common difference", () => {
-      const seq = Arithmetic(5, -1);
-      expect(seq.nextN(5)).toEqual([5, 4, 3, 2, 1]);
-    });
+  it("allows you to set negative values for the common difference", () => {
+    const seq = Arithmetic(5, -1);
+    expect(seq.nextN(5)).toEqual([5, 4, 3, 2, 1]);
   });
 });
--- src/sequenceTypes/geometric.spec.ts
+++ src/sequenceTypes/geometric.spec.ts
@@ -1,25 +1,23 @@
 import Geometric from "./geometric";
 
 describe("Geometric Sequences", () => {
-  describe("generate", () => {
-    it("creates an geometric sequence", () => {
-      const seq = Geometric(1, 2);
-      expect(seq.nextN(5)).toEqual([1, 2, 4, 8, 16]);
-    });
+  it("creates an geometric sequence", () => {
+    const seq = Geometric(1, 2);
+    expect(seq.nextN(5)).toEqual([1, 2, 4, 8, 16]);
+  });
 
-    it("allows you to specify a start number", () => {
-      const seq = Geometric(2, 2);
-      expect(seq.nextN(5)).toEqual([2, 4, 8, 16, 32]);
-    });
+  it("allows you to specify a start number", () => {
+    const seq = Geometric(2, 2);
+    expect(seq.nextN(5)).toEqual([2, 4, 8, 16, 32]);
+  });
 
-    it("allows you to specify a common ratio", () => {
-      const seq = Geometric(1, 3);
-      expect(seq.nextN(5)).toEqual([1, 3, 9, 27, 81]);
-    });
+  it("allows you to specify a common ratio", () => {
+    const seq = Geometric(1, 3);
+    expect(seq.nextN(5)).toEqual([1, 3, 9, 27, 81]);
+  });
 
-    it("allows you to set less-than-1 values for the common ratio", () => {
-      const seq = Geometric(16, 0.5);
-      expect(seq.nextN(5)).toEqual([16, 8, 4, 2, 1]);
-    });
+  it("allows you to set less-than-1 values for the common ratio", () => {
+    const seq = Geometric(16, 0.5);
+    expect(seq.nextN(5)).toEqual([16, 8, 4, 2, 1]);
   });
 });
