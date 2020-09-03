3183a93f365733513c4866fcb0b3ef73ed48a8c1
Mike Hogarth <mikehogarth20@hotmail.com>
2020-09-03 23:03:59 +0100

    Update config/build

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export { default as Arithmetic } from "./arithmetic";
+export { default as Collatz } from "./collatz";
+export { default as Fibonacci } from "./fibonacci";
+export { default as Geometric } from "./geometric";
+export { default as Power } from "./power";
+export { default as Prime } from "./prime";
+export { default as Triangle } from "./triangle";
