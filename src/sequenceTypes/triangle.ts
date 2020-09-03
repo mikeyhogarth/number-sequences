ae8226bf0bf1b6c5fa6996e12102904c9ccf0285
Ben Mulhern <ben@benmulhern.co.uk>
2020-09-03 10:14:22 +0100

    Rework triangle numbers without a helper array

--- src/sequenceTypes/triangle.ts
+++ src/sequenceTypes/triangle.ts
@@ -13,11 +13,18 @@ const Triangle = (): Sequence => {
 };
 
 // Generator
-function* generator(current: number, prev: Array<number> = []): Generator {
-  prev.push(current);
+// The general formula for the nth triangle number is n*(n + 1)/2
+// If the current number is x, then x = n*(n + 1)/2
+// and so with a bit of algebra and the quadratic formula (only taking the positive answer)
+// n = (1 + 8*x)^(1/2)/2
+// So we can work out from the current value that it's the nth value, and then just
+// add n + 1 to get the next term
+// e.g. if current term is 10, then the above formula tells us it's the 4th term,
+// so we add 5 to get the 5th term = 15
+function* generator(current: number): Generator {
   yield current;
-  const next = prev[prev.length - 1] + prev.length + 1;
-  yield* generator(next, prev);
+  const n = (Math.sqrt(1 + 8 * current) - 1) / 2;
+  yield* generator(current + n + 1);
 }
 
 export default Triangle;
