15ae0af9cdaa553eaaf7eae4ce6566dee9e79bb3
Mike Hogarth <mikehogarth20@hotmail.com>
2020-09-03 20:10:15 +0100

    Exopse public API

---
+++ index.ts
@@ -0,0 +1,7 @@
+export { default as Arithmetic } from "./src/arithmetic";
+export { default as Collatz } from "./src/collatz";
+export { default as Fibonacci } from "./src/fibonacci";
+export { default as Geometric } from "./src/geometric";
+export { default as Power } from "./src/power";
+export { default as Prime } from "./src/prime";
+export { default as Triangle } from "./src/triangle";
